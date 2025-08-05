4dcd8fa6aa61a894a049c88462dbb8fe218f054d
flashlight <maxineidel@gmail.com>
2025-08-05 16:59:10 +0200

    test action

--- getLinks.sh
+++ getLinks.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-source .env
+set -e
+
+#source .env
+
+echo $CHANNEL_ID
 
 URL_REGEX='/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/'
 
