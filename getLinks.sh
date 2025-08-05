1cf082e262d4e5a5116575dab7f4110d682a533c
flashlight <maxineidel@gmail.com>
2025-08-05 16:32:31 +0200

    test action

--- getLinks.sh
+++ getLinks.sh
@@ -37,7 +37,7 @@ function deleteMessage() {
 }
 
 echo "pls"
-echo getMessages
+echo $(getMessages)
 getMessages | jq -c '.[]' | while read -r item; do
   echo "hallo1"
   id=$(jq -r '.id' <<< "$item")
